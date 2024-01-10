090a11a16013b6510dc201f53593bb9045c7b8d8
Yasiru Chamuditha <109359904+yasiruchamuditha@users.noreply.github.com>
2024-01-11 02:50:22 +0530

    initial commit 1.create MainActivity[Loading PAge] with animation 2.create Login_Page

---
+++ app/src/main/java/com/example/cdrb_01/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.cdrb_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, Login_Page.class));
+                finish();
+            }
+        },5000);
+    }
+}
